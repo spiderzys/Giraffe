48ad59678d4e3db70781f012cba95b678b63a1b0
spiderzys <spiderzys@hotmail.com>
2016-07-07 12:02:48 -0400

    Initial Commit

---
+++ Giraffe/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Giraffe
+//
+//  Created by YANGSHENG ZOU on 2016-07-07.
+//  Copyright © 2016 YANGSHENG ZOU. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Giraffe/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Giraffe
+//
+//  Created by YANGSHENG ZOU on 2016-07-07.
+//  Copyright © 2016 YANGSHENG ZOU. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
